9c9242255b3ccf430c6dcf8d5f7bd2d5df4467ef
Dobri <Dobri@DESKTOP-33M0NFR>
2020-12-09 11:52:59 +0200

    DominoPlayer

---
+++ DominoTile/src/DominoTile/DominoPlayer.java
@@ -0,0 +1,55 @@
+package DominoTile;
+
+public class DominoPlayer {
+		private String nickname;
+		private DominoTile[] tiles;
+		private int tilesInHands;
+		private DominoTable table;
+		
+		public void setNickname(String nickname) {
+			if (nickname!=null && nickname!="") {
+				
+				this.nickname = nickname;
+			}
+		}
+		
+		
+		public DominoPlayer(String nickname,  DominoTile[] tiles,int tilesInHands,DominoTable table) {
+				this.setNickname(nickname);
+				this.tiles = tiles;
+				this.tilesInHands = tilesInHands;
+				this.table = table;
+		}
+		
+		public String getNickname() {
+			return this.nickname;
+		}
+		
+		public int getTilesInHand() {
+			return this.tilesInHands;
+		}
+		
+		public DominoTable getTable() {
+			return this.table;
+		}
+		
+		private int getTileIndex(DominoTile t) {
+			for (int i = 0; i < tiles.length; i++) {
+				if (t==tiles[i]) {
+					return i;
+				}
+			}
+			return -1;
+			
+		}
+		public void addTileAtLeft(DominoTile t) {
+			if (getTileIndex(t)!=-1) {
+				
+				
+				this.tiles[getTileIndex(t)] = null;
+				this.tilesInHands --;
+				}
+			}
+			
+		}
+}
